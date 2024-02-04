674ea39b8628d43fc7566ae5721336c0d3e2c9e7
Erik Golinelli <erik@codekraft.it>
2024-02-04 14:29:15 +0100

    wip lexer - countWordOccurrences

--- src/lexer.ts
+++ src/lexer.ts
@@ -1,16 +1,29 @@
 import { convert } from 'html-to-text'
 import { LEXER_TEXT_EMPTY, LEXER_TEXT_NOT_STRING } from './const'
+import { B8CONFIG, TOKENS } from './types'
 
 export class Lexer {
-	tokens: string[] | null
-	private config: {}
+	tokens: TOKENS | null = {}
+	private config: B8CONFIG['lexer']
 	private processedText: string | null
 
-	constructor(config = {}) {
+	constructor(config: B8CONFIG['lexer']) {
 		this.config = config
 
 		this.tokens = null
 		this.processedText = null
+	}
+
+	countWordOccurrences(wordsArray: string[]): TOKENS {
+		const wordCount: TOKENS = {}
+		wordsArray.forEach((word) => {
+			if (wordCount[word]) {
+				wordCount[word] += 1
+			} else {
+				wordCount[word] = 1
+			}
+		})
+		return wordCount
 	}
 
 	/**
@@ -19,28 +32,24 @@ export class Lexer {
 	 * @param {string} text - the input text to tokenize
 	 * @return {string[]} the array of tokens generated from the input text
 	 */
-	getTokens(text: string | null) {
+	getTokens(text: string | null): TOKENS {
 		if (typeof text !== 'string') {
-			throw new Error(LEXER_TEXT_NOT_STRING)
+			console.error(LEXER_TEXT_NOT_STRING)
 		}
 
 		// Convert HTML to plain text
 		text = convert(text)
 
 		if (text.trim() === '') {
-			throw new Error(LEXER_TEXT_EMPTY)
+			console.error(LEXER_TEXT_EMPTY)
 		}
 
 		// Tokenization logic...
 		const tokens = text.split(/\s+/)
 
-		this.tokens = tokens
+		this.tokens = this.countWordOccurrences(tokens)
 		this.processedText = text
 
-		return tokens
-	}
-
-	isValid(token: unknown) {
-		return token && typeof token === 'string'
+		return this.tokens
 	}
 }
