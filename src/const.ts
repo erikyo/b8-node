f833f52206d0d07c0b01e0b917a7a491757b7fe7
Erik Golinelli <erik@codekraft.it>
2024-02-04 14:27:55 +0100

    updated types

--- src/const.ts
+++ src/const.ts
@@ -1,4 +1,6 @@
-export const DB_VERSION = 3
+import { B8CONFIG } from './types'
+
+export const DB_VERSION: number = 1
 export const CLASSIFIER_TEXT_MISSING = 'CLASSIFIER_TEXT_MISSING'
 export const TRAINER_TEXT_MISSING = 'TRAINER_TEXT_MISSING'
 export const TRAINER_CATEGORY_MISSING = 'TRAINER_CATEGORY_MISSING'
@@ -6,3 +8,58 @@ export const TRAINER_CATEGORY_FAIL = 'TRAINER_CATEGORY_FAIL'
 export const LEXER_TEXT_NOT_STRING = 'LEXER_TEXT_NOT_STRING'
 export const LEXER_TEXT_EMPTY = 'LEXER_TEXT_EMPTY'
 export const defaultPath = 'b8.db'
+
+export const INTERNALS = 'b8*texts'
+
+export const configDefaults: B8CONFIG = {
+	min_dev: 0.01,
+	rob_s: 0.5,
+	rob_x: 0.5,
+	use_relevant: 0.95,
+	lexer: {
+		min_size: 3,
+		max_size: 30,
+		get_uris: true,
+		get_html: true,
+		get_bbcode: false,
+		allow_numbers: false,
+		stopwords: [
+			'about',
+			'an',
+			'are',
+			'as',
+			'at',
+			'be',
+			'by',
+			'com',
+			'for',
+			'from',
+			'how',
+			'in',
+			'is',
+			'it',
+			'of',
+			'on',
+			'or',
+			'that',
+			'the',
+			'this',
+			'to',
+			'was',
+			'what',
+			'when',
+			'where',
+			'who',
+			'will',
+			'with',
+			'www',
+		],
+	},
+	degenerator: {
+		multibyte: true,
+		encoding: 'UTF-8',
+	},
+	storage: {
+		dbPath: ':memory:',
+	},
+}
--- src/types.ts
+++ src/types.ts
@@ -1,25 +1,25 @@
 import { Database } from 'sqlite3'
 
 export type B8CONFIG = {
-	min_dev?: number
-	rob_s?: number
-	rob_x?: number
-	use_relevant?: number
-	dbPath?: string
-	lexer?: {
+	min_dev: number
+	rob_s: number
+	rob_x: number
+	use_relevant: number
+	lexer: {
 		min_size?: number
 		max_size?: number
 		get_uris?: boolean
 		get_html?: boolean
 		get_bbcode?: boolean
 		allow_numbers?: boolean
+		stopwords?: string[]
 	}
-	degenerator?: {
+	degenerator: {
 		multibyte?: boolean
 		encoding?: string
 	}
-	storage?: {
-		dbPath?: string
+	storage: {
+		dbPath: string
 	}
 }
 
@@ -30,9 +30,15 @@ export interface DATASET {
 	totalUnlearned: number
 }
 
-export interface ROW extends Database {
+export interface TOKEN extends Database {
 	pos: number
 	neg: number
+}
+
+export type TOKENS = Record<string, number>
+
+export interface ROW extends TOKEN {
+	token: string
 }
 
 export type ROWS = {
